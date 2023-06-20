4c9bb5579e0e1bea561bddb58ac5459aa0c7fdb0
unknown <riteshv197@gmail.com>
2023-06-20 12:26:40 +0530

    Hyperparameter Tunning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_tranformation import DataTransformation
-from src.components.data_tranformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
-from src.components.model_trainer import ModelTrainingConfig
 
 @dataclass
 class DataIngesgionConfig:
